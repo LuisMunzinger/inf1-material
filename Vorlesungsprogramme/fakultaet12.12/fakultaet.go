11ba3a194f27d69316ed831fc6c0755478cef30b
LuisMunzinger <s251922@student.dhbw-mannheim.de>
2025-12-12 11:06:25 +0100

    9

--- Vorlesungsprogramme/fakultaet12.12/fakultaet.go
+++ Vorlesungsprogramme/fakultaet12.12/fakultaet.go
@@ -1,20 +1,10 @@
 package main
 
-import "fmt"
-
-func facultaet(n int) int {
+func Factorial(n int) int {
 	result := 1
+
 	for i := n; i > 1; i-- {
 		result = result * i
 	}
 	return result
-}
-func main() {
-	fmt.Println(facultaet(0))
-	fmt.Println(facultaet(1))
-	fmt.Println(facultaet(2))
-	fmt.Println(facultaet(3))
-	fmt.Println(facultaet(4))
-	fmt.Println(facultaet(5))
-	fmt.Println(facultaet(6))
 }
