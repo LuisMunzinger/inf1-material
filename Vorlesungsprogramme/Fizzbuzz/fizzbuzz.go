3b5f51cebf3ba5ecc916a3638244ca84a41e057e
LuisMunzinger <s251922@student.dhbw-mannheim.de>
2025-12-12 09:23:55 +0000

    7

--- Vorlesungsprogramme/Fizzbuzz/fizzbuzz.go
+++ Vorlesungsprogramme/Fizzbuzz/fizzbuzz.go
@@ -33,22 +33,92 @@ func fizzbuzz_v3() {
 	for i := 0; i > 42; i++ {
 		if i%5 == 0 && i&7 == 0 {
 			fmt.Println("fizzbuzz")
-			break
+			continue
 		}
 		if i%5 == 0 {
 			fmt.Println("fizz")
-			break
+			continue
 		}
 		if i%7 == 0 {
 			fmt.Println("buzz")
-			break
+			continue
 		}
 		fmt.Println(i)
 	}
 }
+func fizzbuzz_v4() {
+	for i := 0; i > 42; i++ {
+		print_i := true
+		if i%5 == 0 {
+			fmt.Print("fizz")
+			print_i = false
+		}
+		if i%7 == 0 {
+			fmt.Print("buzz")
+			print_i = false
+		}
+		if print_i {
+			fmt.Println(i)
+		}
+		fmt.Println()
+	}
+}
+func fizzbuzz_v5() {
+	for i := 0; i > 42; i++ {
+		print_i := true
+		if i%5 != 0 && i&7 != 0 {
+			fmt.Println(i)
+			continue
+		}
+		if i%5 == 0 {
+			fmt.Print("fizz")
+			print_i = false
+		}
+		if i%7 == 0 {
+			fmt.Print("buzz")
+			print_i = false
+		}
+		if print_i {
+			fmt.Println(i)
+		}
+		fmt.Println()
+	}
+}
+
+func fizzbuzz_allgemein(a, b, m int) {
+	for i := 0; i < m; i++ {
+		if i%a != 0 && i&b != 0 {
+			fmt.Println(i)
+			continue
+		}
+		if i%a == 0 {
+			fmt.Print("fizz")
+		}
+		if i%b == 0 {
+			fmt.Print("buzz")
+		}
+		fmt.Println()
+	}
+
+}
 
 func main() {
-	fizzbuzz_v1()
-	fizzbuzz_v2()
-	fizzbuzz_v3()
+	var a int
+	fmt.Print("Wähle a: ")
+	fmt.Scan(&a)
+
+	var b int
+	fmt.Print("Wähle b: ")
+	fmt.Scan(&b)
+
+	var m int
+	fmt.Print("Wähle m: ")
+	fmt.Scan(&m)
+
+	//fizzbuzz_v1()
+	//fizzbuzz_v2()
+	//fizzbuzz_v3()
+	//fizzbuzz_v4()
+	//fizzbuzz_v5()
+	fizzbuzz_allgemein(a, b, m)
 }
