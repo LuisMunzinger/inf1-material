212fe4a70406f57504e372f8474154b076e7fae2
LuisMunzinger <s251922@student.dhbw-mannheim.de>
2025-12-12 10:56:17 +0100

    8

--- Vorlesungsprogramme/GuessingGame/main/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "inf1-material/Vorlesungsprogramme/GuessingGame"
-
-func main(){
-	GuessingGame.GuessingGame()
-}
---
+++ Vorlesungsprogramme/GuessingGame05.12/main/main.go
@@ -0,0 +1,7 @@
+package main
+
+import GuessingGame "inf1-material/Vorlesungsprogramme/GuessingGame05.12"
+
+func main() {
+	GuessingGame.GuessingGame()
+}
