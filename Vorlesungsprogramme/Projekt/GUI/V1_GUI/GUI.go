ce1a9f27d10cc36feeee83003615c64c89c5d9e7
LuisMunzinger <s251922@student.dhbw-mannheim.de>
2026-01-16 14:44:49 +0100

    26

--- Vorlesungsprogramme/Projekt/GUI/V1_GUI/GUI.go
+++ Vorlesungsprogramme/Projekt/GUI/V1_GUI/GUI.go
@@ -1,18 +1,23 @@
 package main
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Hello")
+	w := a.NewWindow("Button Beispiel")
 
-	w.SetContent(widget.NewLabel("Luis"))
-
-	btn := widget.NewButton("Klick mich", func() { println("Button wurde geklickt") })
-
-	w.SetContent(btn)
+	content := container.NewGridWrap(
+		fyne.NewSize(150, 50),
+		widget.NewButton("Ja", func() { println("Ja") }),
+		widget.NewButton("Nein", func() { println("Nein") }),
+		widget.NewButton("Abbruch", func() { println("Abbruch") }),
+	)
+	w.SetContent(content)
+	//w.SetContent(container.NewVBox(widget.NewLabel("Hello Fyne!")))
 	w.ShowAndRun()
 }
