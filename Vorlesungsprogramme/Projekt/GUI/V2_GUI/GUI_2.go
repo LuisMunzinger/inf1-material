e8dade25b204745e2420ef85a7d619936cb32cb4
LuisMunzinger <s251922@student.dhbw-mannheim.de>
2026-01-23 09:58:42 +0100

    28

--- Vorlesungsprogramme/Projekt/GUI/V2_GUI/GUI_2.go
+++ Vorlesungsprogramme/Projekt/GUI/V2_GUI/GUI_2.go
@@ -11,13 +11,21 @@ func main() {
 	a := app.New()
 	w := a.NewWindow("GUI V2")
 
+	label := widget.NewLabel("Wie heist du?")
+	label2 := widget.NewLabel("")
+
+	entry := widget.NewEntry()
+	entry.SetPlaceHolder("Name eingeben...")
+
 	content := container.NewGridWrap(
 		fyne.NewSize(150, 50),
-		widget.NewButton("Ja", func() { println("Ja") }),
-		widget.NewButton("Nein", func() { println("Nein") }),
-		widget.NewButton("Abbruch", func() { w.Close() }),
+		widget.NewButton("Fertig", func() { label2.SetText("Hallo " + entry.Text) }),
+		widget.NewButton("Close", func() { w.Close() }),
 	)
-	label := widget.NewLabel("HI")
-	w.SetContent(container.NewVBox(label, content))
+
+	w.SetContent(container.NewVBox(label, label2, entry, content))
+	w.SetContent(
+		(container.NewWithoutLayout()),
+	)
 	w.ShowAndRun()
 }
