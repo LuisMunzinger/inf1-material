f6fcb74ffea11b5897694cf7f42f6b5dc521b48e
LuisMunzinger <s251922@student.dhbw-mannheim.de>
2026-01-23 10:52:00 +0100

    29

--- Vorlesungsprogramme/26.01.23/Quicksorte/Qsort_test.go
+++ Vorlesungsprogramme/26.01.23/Quicksorte/Qsort_test.go
@@ -3,13 +3,13 @@ package Quicksorte
 import "fmt"
 
 //soriere die Liste l mit dem Verfahren Quicksorte
-func Qsort(l []int){
+func Qsort(l []int) {
 
 	//sonderfall: Die Liste ist Leer oder hat nur eine Element
-	if len(l) <= 1{
+	if len(l) <= 1 {
 		return
 	}
-	
+
 	pivot := l[0]
 
 	left := []int{}
@@ -29,22 +29,22 @@ func Qsort(l []int){
 	Qsort(right)
 
 	//Elemente in die ursprüngliche Liste zurückkopieren
-	if i, el := range left {
+	for i, el := range left {
 		l[i] = el
 	}
 	l[len(left)] = pivot
-	if i, el := range right {
+	for i, el := range right {
 		l[i+len(left)+1] = el
 	}
 }
 
 func ExampleQsort() {
-	l1 := []int{17,25,22,3,15,4,35,105,42,1}
+	l1 := []int{17, 25, 22, 3, 15, 4, 35, 105, 42, 1}
 
 	Qsort(l1)
 
 	fmt.Println(l1)
-	
+
 	// Output:
 	// [1 3 4 15 17 22 25 35 42 105]
 }
--- Vorlesungsprogramme/Projekt/GUI/V2_GUI/GUI_2.go
+++ Vorlesungsprogramme/Projekt/GUI/V2_GUI/GUI_2.go
@@ -4,28 +4,41 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("GUI V2")
-
-	label := widget.NewLabel("Wie heist du?")
+	//---------------------------------------------------------------------------------------------------
+	label1 := widget.NewLabel("Wie heißt du?")
 	label2 := widget.NewLabel("")
 
 	entry := widget.NewEntry()
 	entry.SetPlaceHolder("Name eingeben...")
 
-	content := container.NewGridWrap(
-		fyne.NewSize(150, 50),
-		widget.NewButton("Fertig", func() { label2.SetText("Hallo " + entry.Text) }),
+	button := widget.NewButton("Fertig", func() { label2.SetText("Hallo " + entry.Text) })
+
+	head := container.NewVBox(
+		label1, label2, entry, button,
+	)
+	//---------------------------------------------------------------------------------------------------
+	bottom := container.NewHBox(
+		layout.NewSpacer(),
 		widget.NewButton("Close", func() { w.Close() }),
 	)
-
-	w.SetContent(container.NewVBox(label, label2, entry, content))
+	//---------------------------------------------------------------------------------------------------
 	w.SetContent(
-		(container.NewWithoutLayout()),
+		container.NewBorder(
+			head,   // oben fix
+			bottom, // unten fix
+			nil,    // links fixiert
+			nil,    // rechts fixiert
+			nil,    // mitte flexibel
+		),
 	)
+
+	w.Resize(fyne.NewSize(400, 300))
 	w.ShowAndRun()
 }
