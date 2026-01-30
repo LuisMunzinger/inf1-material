b44eb46b7bbd360ea7c8f875c81fefde2932e48e
LuisMunzinger <s251922@student.dhbw-mannheim.de>
2026-01-30 11:58:49 +0100

    44

--- Vorlesungsprogramme/26.01.30/tennis/tennis_test.go
+++ Vorlesungsprogramme/26.01.30/tennis/tennis_test.go
@@ -2,38 +2,69 @@ package tennis
 
 import "fmt"
 
+func ExamplePrint() {
+	s1 := NewSet()
+	s1.NewGame()
+
+	s1.Score(1)
+	s1.Print()
+
+	s1.Score(2)
+	s1.Print()
+
+	s1.Score(1)
+	s1.Print()
+
+	s1.Score(2)
+	s1.Print()
+
+	s1.Score(1)
+	s1.Print()
+
+	// Output:
+}
 func Example() {
 	n1 := "Alcaraz"
 	n2 := "Zverev"
 
-	g1 := Game{30, 40}
-	g2 := Game{40, 30}
-	g3 := Game{0, 40}
-	g4 := Game{40, 15}
-	g5 := Game{40, 15}
-	g6 := Game{40, 15}
-	g7 := Game{40, 50}
-	g8 := Game{40, 30}
-	g9 := Game{30, 40}
-	g10 := Game{40, 30}
+	s1 := NewSet()
 
-	s1 := Set{[]Game{g1, g2, g3, g4, g5, g6, g7, g8, g9, g10}}
+	g1 := Game{30, 40}
+	s1.AddGame(g1)
+
+	g2 := Game{40, 30}
+	s1.AddGame(g2)
+	g3 := Game{0, 40}
+	s1.AddGame(g3)
+
+	g4 := Game{40, 15}
+	s1.AddGame(g4)
+
+	g5 := Game{40, 15}
+	s1.AddGame(g5)
+
+	g6 := Game{40, 15}
+	s1.AddGame(g6)
+
+	g7 := Game{40, 50}
+	s1.AddGame(g7)
+
+	g8 := Game{40, 30}
+	s1.AddGame(g8)
+
+	g9 := Game{30, 40}
+	s1.AddGame(g9)
+
+	g10 := Game{40, 30}
+	s1.AddGame(g10)
 
 	fmt.Println(s1)
+	fmt.Println(s1.games)
 	fmt.Println(s1.games[0])
 	fmt.Println(s1.games[0].p1)
 
 	fmt.Printf("Spiel: %s gegen %s\n", n1, n2)
-	fmt.Printf("Ergebns von Spiel 1: %d : %d\n", g1.p1, g1.p2)
-	fmt.Printf("Ergebns von Spiel 2: %d : %d\n", g2.p1, g2.p2)
-	fmt.Printf("Ergebns von Spiel 3: %d : %d\n", g3.p1, g3.p2)
-	fmt.Printf("Ergebns von Spiel 4: %d : %d\n", g4.p1, g4.p2)
-	fmt.Printf("Ergebns von Spiel 5: %d : %d\n", g5.p1, g5.p2)
-	fmt.Printf("Ergebns von Spiel 6: %d : %d\n", g6.p1, g6.p2)
-	fmt.Printf("Ergebns von Spiel 7: %d : %d\n", g7.p1, g7.p2)
-	fmt.Printf("Ergebns von Spiel 8: %d : %d\n", g8.p1, g8.p2)
-	fmt.Printf("Ergebns von Spiel 9: %d : %d\n", g9.p1, g9.p2)
-	fmt.Printf("Ergebns von Spiel 10: %d : %d\n", g10.p1, g10.p2)
+	s1.PrintResults()
 
 	// Output:
 
