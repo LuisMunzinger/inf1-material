b642d3ce82e7819f42bf0001c3433a48feff57c9
LuisMunzinger <s251922@student.dhbw-mannheim.de>
2026-01-30 10:08:03 +0100

    41

--- Vorlesungsprogramme/26.01.30/Duration/ZeitUmrechnen_test.go
+++ Vorlesungsprogramme/26.01.30/Duration/ZeitUmrechnen_test.go
@@ -4,9 +4,12 @@ import "fmt"
 
 type Duration uint
 
+// Duration als Sekunden festlegen
 func Seconds(m int) Duration {
 	return Duration(m)
 }
+
+//------------------------------------------------------------------------------------
 
 // Minutes erwartet ein int, das als Minuten interpretiert wird.
 // Liefert eine Duration für diese Minuten-Anzahl.(Anzahl der Sekunden)
@@ -18,6 +21,8 @@ func Houers(m int) Duration {
 	return Duration(m * 60 * 60)
 }
 
+//-------------------------------------------------------------------------------------
+
 // ToSeconds liefert die Duration als Secunden
 func (s Duration) ToSeconds() int {
 	return int(s)
@@ -28,22 +33,35 @@ func (s Duration) ToMinutes() int {
 	return int(s) / 60
 }
 
+// ToHouers liefert die Duration als Stunden
+func (s Duration) ToHouers() int {
+	return int(s) / 60 / 60
+}
+
+//------------------Duration nicht mehr verwendet (müssen es nicht mehr wissen)----------------------------------
+
 // SecondsToMinutes erwartet eine Sekunden-Anzahl
 // und Liefert die entsprechende Minuten.
 func SecondsToMinutes(s int) int {
 	return int(Seconds(s).ToMinutes())
 }
 
+func SecondsToHouers(s int) int {
+	return int(Seconds(s).ToHouers())
+}
+
 func Example() {
 	fmt.Println(Seconds(60))
 	fmt.Println(Minutes(60))
-	fmt.Println(Houers(60))
-	fmt.Println(SecondsToMinutes(60))
+	fmt.Println(Houers(5))
+	fmt.Println(SecondsToMinutes(120))
+	fmt.Println(SecondsToHouers(3600))
 
 	// Output:
 	// 60
 	// 3600
-	// 216000
+	// 18000
+	// 2
 	// 1
 
 }
