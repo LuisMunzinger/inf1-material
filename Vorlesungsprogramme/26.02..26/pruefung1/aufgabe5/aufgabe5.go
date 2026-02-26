70cb502d9650a40fb06c1144d6d2a1d449fb857c
LuisMunzinger <s251922@student.dhbw-mannheim.de>
2026-02-26 16:34:58 +0100

    93

---
+++ Vorlesungsprogramme/26.02..26/pruefung1/aufgabe5/aufgabe5.go
@@ -0,0 +1,26 @@
+package aufgabe5
+
+/*
+AUFGABENSTELLUNG: Vervollständigen Sie die Methode GreaterThan für
+den Datentyp Card, der unten definiert ist.
+MAX. PUNKTE: 5
+*/
+
+// GreaterThan prüft, ob c nach den Skat-Regeln größer ist als other.
+// Dies ist dann der Fall, wenn die Farben (Suit) gleich sind und
+// der Rang größer als bei other ist.
+func (c Card) GreaterThan(other Card) bool {
+	if c.Suit == other.Suit {
+		if c.Rank > other.Rank {
+			return true
+		}
+	}
+
+	return false
+}
+
+// Vorgegebener Datentyp
+type Card struct {
+	Suit int //Farbe
+	Rank int
+}
---
+++ Vorlesungsprogramme/26.02..26/pruefung1/aufgabe6/aufgabe6.go
@@ -0,0 +1,31 @@
+package aufgabe6
+
+import "slices"
+
+/*
+AUFGABENSTELLUNG: Vervollständigen Sie die Funktion SymmetricDifference.
+MAX. PUNKTE: 10
+*/
+
+// SymmetricDifference erwartet zwei int-Listen l1 und l2.
+// Die Funktion liefert eine int-Liste mit den Elementen,
+// die in einer, aber nicht in beiden Listen vorhanden sind.
+//
+// Die Elemente kommen dabei in der gleichen Reihenfolge vor, wie in den
+// Ursprungslisten, mehrfach vorkommende Elemente werden entsprechend wiederholt.
+// Die Elemente aus l1 kommen vor denen aus l2 in der Ergebnisliste vor.
+func SymmetricDifference(l1, l2 []int) []int {
+	ergebnis := []int{}
+	
+	for i := 0, i < len(l1), i++{
+		if slices.Contains(l2,l1[i]) == false{
+			ergebnis = append(ergebnis,l1[i])
+		}
+	}
+	for i := 0, i < len(l2), i++{
+		if slices.Contains(l1,l2[i]) == false{
+			ergebnis = append(ergebnis,l2[i])
+		}
+	}
+	return ergebnis
+}
