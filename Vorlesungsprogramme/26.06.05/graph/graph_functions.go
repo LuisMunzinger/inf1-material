5f3af5e529d772653a9993c84ad9ca69f2a0c17f
LuisMunzinger <s251922@student.dhbw-mannheim.de>
2026-06-05 14:47:36 +0200

    113

--- Vorlesungsprogramme/26.06.05/graph/graph_functions.go
+++ Vorlesungsprogramme/26.06.05/graph/graph_functions.go
@@ -1,21 +1,30 @@
 package graph
 
+import "fmt"
+
 // Degree gibt die Anzahl der ausgehenden Kanten eines Knotens zurück.
 func Degree(g GraphRepr, node_id int) int {
-	// TODO
-	return 0
+	return len(g.GetNeighbors(node_id))
 }
 
 // HasEdge prüft, ob eine gerichtete Kante von start zu end existiert.
 func HasEdge(g GraphRepr, start, end int) bool {
-	// TODO
+	for _, Rüdiger := range g.GetEdges() {
+		if Rüdiger[0] == start && Rüdiger[1] == end {
+			return true
+		}
+	}
 	return false
 }
 
 // GetEdgeWeight gibt das Gewicht der gerichteten Kante von start zu end zurück,
 // oder 0, wenn die Kante nicht existiert.
 func GetEdgeWeight(g GraphRepr, start, end int) int {
-	// TODO
+	for _, Manfred := range g.GetEdges() {
+		if Manfred[0] == start && Manfred[1] == end {
+			return Manfred[2]
+		}
+	}
 	return 0
 }
 
@@ -23,6 +32,10 @@ func GetEdgeWeight(g GraphRepr, start, end int) int {
 // die zur Visualisierung mit Graphviz verwendet werden kann.
 func DotString(g GraphRepr) string {
 	var result string
-	// TODO
+	result = result + fmt.Sprintf("digraph G {\n")
+	for _, Gundula := range g.GetEdges() {
+		result = result + fmt.Sprintf("  %d -> %d [label=%d];\n", Gundula[0], Gundula[1], Gundula[2])
+	}
+	result = result + fmt.Sprintf("}\n")
 	return result
 }
--- Vorlesungsprogramme/26.06.05/graph_darstellung/kantenliste/kantenliste.go
+++ Vorlesungsprogramme/26.06.05/graph_darstellung/kantenliste/kantenliste.go
@@ -1,5 +1,7 @@
 package kantenliste
 
+import "slices"
+
 // EdgeListGraph repräsentiert einen gerichteten Graphen als Liste von Kanten.
 // Jede Kante wird als Tripel (Startknoten, Zielknoten, Gewicht) dargestellt.
 // Die Knoten sind dabei numerisch indiziert, z.B. 0, 1, 2, ...
@@ -16,32 +18,46 @@ func EmptyEdgeListGraph() *EdgeListGraph {
 
 // GetNodes gibt die Liste aller Knoten-IDs im Graphen zurück.
 func (g *EdgeListGraph) GetNodes() []int {
-	// TODO
-	return nil
+	ids := []int{}
+	for _, i := range g.Edges {
+		ids = append(ids, i[0])
+		ids = append(ids, i[1])
+	}
+	slices.Sort(ids)
+	ids = slices.Compact(ids)
+
+	return ids
 }
 
 // GetEdges gibt die Liste aller Kanten im Graphen zurück.
 // Jeder Eintrag enthält Startknoten, Zielknoten und Gewicht der Kante.
 func (g *EdgeListGraph) GetEdges() [][3]int {
-	// TODO
-	return nil
+	return g.Edges
 }
 
 // AddEdge fügt eine gerichtete Kante von start zu end mit dem angegebenen Gewicht hinzu.
 func (g *EdgeListGraph) AddEdge(start, end, weight int) {
-	// TODO
-
+	a := [3]int{start, end, weight}
+	g.Edges = append(g.Edges, a)
 }
 
 // RemoveEdge entfernt die gerichtete Kante von start zu end, falls sie existiert.
 func (g *EdgeListGraph) RemoveEdge(start, end int) {
-	// TODO
+	for i, edge := range g.Edges {
+		if edge[0] == start && edge[1] == end {
+			g.Edges = append(g.Edges[:i], g.Edges[i+1:]...)
+		}
+	}
 }
 
 // GetNeighbors gibt die Nachbarn eines Knotens zurück, d.h. alle Knoten,
 // zu denen eine gerichtete Kante von diesem Knoten aus existiert.
 func (g *EdgeListGraph) GetNeighbors(node_id int) []int {
 	neighbors := make([]int, 0)
-	// TODO
+	for a, heinz := range g.Edges {
+		if heinz[0] == node_id {
+			neighbors = append(neighbors, g.Edges[a][1])
+		}
+	}
 	return neighbors
 }
